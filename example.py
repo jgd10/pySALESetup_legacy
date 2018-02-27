21d8981275cfa951738549e28474af3998603d12
jgd10 <jamesgordonderrick@gmail.com>
2018-02-27 23:37:35 +0000

    import numpy accidentally deleted-fixed; tidied example.py

--- example.py
+++ example.py
@@ -2,34 +2,26 @@ import pySALESetup as pss
 import numpy as np
 
 g = []
-#grain = pss.Grain()
-#g.append(grain)
-
 mesh = pss.Mesh(X=600,Y=100)
 mesh2 = pss.Mesh(X=600,Y=100)
 
 #grain.view()
 
-
-
-#g[0].place(300,50,m=2,target=mesh)
-#g[1].place(300,60,m=3,target=mesh)
-
 import random
 group1 = pss.Ensemble(mesh)
 for i in range(100):
     rot = random.random()*np.pi
-    ecc = random.random()
+    ecc = random.random()*0.5
     grain = pss.Grain(shape='ellipse',rot=rot,elps_params=[10.,ecc])
     g.append(grain)
-    if i == 0: 
+    if i < 50: 
         grain.insertRandomly(mesh,1)
     else:
         grain.insertRandomwalk(mesh,1)
     group1.add(grain)
 
 
-group1.optimise_materials(np.array([1,2,3,4,5]))
+group1.optimise_materials(np.array([1,2,3,4,5,6,7]))
 
 for x,y,g,m in zip(group1.xc,group1.yc,group1.grains,group1.mats):
     g.place(x,y,m,mesh2)
@@ -37,10 +29,10 @@ for x,y,g,m in zip(group1.xc,group1.yc,group1.grains,group1.mats):
 for x,y,g,m in zip(group1.xc,group1.yc,group1.grains,group1.mats):
     g.radius = g.radius+4
     g.mesh = pss.gen_ellipse(g.radius,g.angle,g.eccentricity) 
-    g.place(x,y,6,mesh2)
-mesh2.fillAll(m=7)
+    g.place(x,y,8,mesh2)
+mesh2.fillAll(m=9)
 
-mesh2.plateVel(0.,1.5e-3,1500.,axis=1)
-mesh2.plateVel(1.5e-3,3.e-3,-1500.,axis=1)
+#mesh2.plateVel(0.,1.5e-3,1500.,axis=1)
+#mesh2.plateVel(1.5e-3,3.e-3,-1500.,axis=1)
 
 mesh2.viewMats()
