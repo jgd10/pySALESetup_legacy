e6d004bbcee7cffd2d962bf762c10410d44d24ab
jgd10 <jgd10@imperial.ac.uk>
2018-04-26 17:12:05 +0100

    regolith_voids now creates a regolith mesh with varying numbers of voids

--- regolith_voids.py
+++ regolith_voids.py
@@ -82,184 +82,120 @@ for r,p in zip(Rs,phi):
 # order grains from largest to smallest
 grains = [g for _,g in sorted(zip(phi,grains))]
 
-groupA = pss.Ensemble(meshA,name='shapevariationENS_A')
-groupB = pss.Ensemble(meshB,name='shapevariationENS_B')
-try:
-    i = 0
-    for g in grains:
-        for f in range(g.targetFreq):
-            g.insertRandomly(meshA, m=1)
-            groupA.add(g,g.x,g.y)
-            g.insertRandomly(meshB, m=1)
-            groupB.add(g,g.x,g.y)
-except KeyboardInterrupt:
-    pass
+
+vf_V = np.array([0.,0.05,0.1,0.15,0.2,0.25])
+vf_V = vf_V[::-1]
+# matrix porosity = 0.5
+#vf_G = 5.*vf_V
+# matrix porosity = 0.6
+vf_G = 2.*vf_V/3. + 1./6.
+# matrix porosity = 0.7
+#vf_G = (2. + 3.*vf_V)/7.
+# matrix porosity = 0.8
+#vf_G = (.25*vf_V+3./8.)
+print vf_V,vf_G
+first = True
+for v_void,v_grain in zip(vf_V,vf_G):
+    if first:
+        v_grain0 = v_grain
+        v_void0 = v_void
+        groupAG = pss.Ensemble(meshA,name='voidvariation_vfG-{:2.2f}_vfV-{:2.2f}_GRAINS_A'.format(v_grain0,v_void0))
+        groupBG = pss.Ensemble(meshB,name='voidvariation_vfG-{:2.2f}_vfV-{:2.2f}_GRAINS_B'.format(v_grain0,v_void0))
+        groupAV = pss.Ensemble(meshA,name='voidvariation_vfG-{:2.2f}_vfV-{:2.2f}_VOIDS_A'.format(v_grain0,v_void0))
+        groupBV = pss.Ensemble(meshB,name='voidvariation_vfG-{:2.2f}_vfV-{:2.2f}_VOIDS_B'.format(v_grain0,v_void0))
+    else:
+        groupAG = pss.Ensemble(meshA,name='voidvariation_vfG-{:2.2f}_vfV-{:2.2f}_GRAINS_A'.format(v_grain0,v_void0),Reload=True)
+        groupBG = pss.Ensemble(meshB,name='voidvariation_vfG-{:2.2f}_vfV-{:2.2f}_GRAINS_B'.format(v_grain0,v_void0),Reload=True)
+        groupAV = pss.Ensemble(meshA,name='voidvariation_vfG-{:2.2f}_vfV-{:2.2f}_VOIDS_A'.format(v_grain0,v_void0),Reload=True)
+        groupBV = pss.Ensemble(meshB,name='voidvariation_vfG-{:2.2f}_vfV-{:2.2f}_VOIDS_B'.format(v_grain0,v_void0),Reload=True)
+    if first:
+        i = 0
+        vfA = 0.
+        vfB = 0.
+        while vfA <= v_grain:
+            g = grains[i]
+            for f in range(g.targetFreq):
+                g.insertRandomly(meshA, m=1)
+                groupAG.add(g,g.x,g.y)
+                vfA = meshA.calcVol(frac=True)
+                if vfA > v_grain: 
+                    break
+            i += 1
+        j = 0
+        while vfB <= v_grain:
+            g = grains[j]
+            for f in range(g.targetFreq):
+                g.insertRandomly(meshB, m=1)
+                groupBG.add(g,g.x,g.y)
+                vfB = meshB.calcVol(frac=True)
+                if vfB > v_grain: break
+            j+= 1
+
+        meshA.fillAll(2)
+        meshB.fillAll(2)
+    
+        vfA = 0.
+        vfB = 0.
+        g = grains[-1]
+        while vfA < v_void:
+            g.insertRandomly(meshA, m=0,mattargets=[2])
+            groupAV.add(g,g.x,g.y)
+            vfA = 1.-meshA.calcVol(frac=True)
+            if vfA > v_void: break
+        while vfB < v_void:
+            g.insertRandomly(meshB, m=0,mattargets=[2])
+            groupBV.add(g,g.x,g.y)
+            vfB = 1.-meshB.calcVol(frac=True)
+            if vfB > v_void: break
+
+        groupAG.optimise_materials(np.array([1,2,3,4,5,6,7,8]))
+        groupBG.optimise_materials(np.array([1,2,3,4,5,6,7,8]))
+        meshA.fillAll(-1)
+        meshB.fillAll(-1)
+    else:
+        pass
+    for xA,yA,gA,mA in zip(groupAG.xc,groupAG.yc,groupAG.grains,groupAG.mats):
+        gA.place(xA,yA,mA,meshA)
+        vfA = meshA.calcVol(frac=True)
+        if vfA > v_grain: break
+    for xB,yB,gB,mB in zip(groupBG.xc,groupBG.yc,groupBG.grains,groupBG.mats):
+        gB.place(xB,yB,mB,meshB)
+        vfB = meshB.calcVol(frac=True)
+        if vfB > v_grain: break
+    
+    meshA.fillAll(9)
+    meshB.fillAll(9)
+    
+    for xA,yA,gA in zip(groupAV.xc,groupAV.yc,groupAV.grains):
+        gA.place(xA,yA,0,meshA,mattargets=[9])
+        vfA = 1.-meshA.calcVol(frac=True)
+        if vfA > v_void: break
+    for xB,yB,gB in zip(groupBV.xc,groupBV.yc,groupBV.grains):
+        gB.place(xB,yB,0,meshB,mattargets=[9])
+        vfB = 1.-meshB.calcVol(frac=True)
+        if vfB > v_void: break
+    if first:
+        groupAG.save()
+        groupBG.save()
+        groupAV.save()
+        groupBV.save()
+    
+    meshA.blanketVel(-1500.,axis=1)
+    meshB.blanketVel(+1500.,axis=1)
+    
+    meshC = pss.combine_meshes(meshA,meshB,axis=1)
+    meshC.top_and_tail()
+    meshC.viewMats()
+    meshC.save(fname='regolith_vfG-{:2.2f}_vfV{:2.2f}_v3000.iSALE'.format(v_grain,v_void),compress=True)
+    meshC.multiplyVels()
+    meshC.save(fname='regolith_vfG-{:2.2f}_vfV{:2.2f}_v1500.iSALE'.format(v_grain,v_void),compress=True)
+    meshC.multiplyVels()
+    meshC.save(fname='regolith_vfG-{:2.2f}_vfV{:2.2f}_v750.iSALE'.format(v_grain,v_void),compress=True)
+    
+    meshA.fillAll(-1)
+    meshB.fillAll(-1)
+    meshC.fillAll(-1)
+    first = False
 
 
-#print groupA.fabricTensor_discs()
-groupA.calcPSD()
 
-groupA.optimise_materials(np.array([1,2,3,4,5,6,7,8]))
-groupB.optimise_materials(np.array([1,2,3,4,5,6,7,8]))
-
-groupA.save()
-groupB.save()
-
-
-meshA.fillAll(-1)
-meshB.fillAll(-1)
-
-for xA,yA,gA,mA,xB,yB,gB,mB in zip(groupA.xc,groupA.yc,groupA.grains,groupA.mats,groupB.xc,groupB.yc,groupB.grains,groupB.mats):
-    gA.place(xA,yA,mA,meshA)
-    gB.place(xB,yB,mB,meshB)
-
-meshA.fillAll(9)
-meshB.fillAll(9)
-
-meshA.blanketVel(-1500.,axis=1)
-meshB.blanketVel(+1500.,axis=1)
-
-meshC = pss.combine_meshes(meshA,meshB,axis=1)
-meshC.top_and_tail()
-meshC.viewMats()
-meshC.save(fname='regolith_circles_v3000.iSALE',compress=True)
-meshC.multiplyVels()
-meshC.save(fname='regolith_circles_v1500.iSALE',compress=True)
-meshC.multiplyVels()
-meshC.save(fname='regolith_circles_v750.iSALE',compress=True)
-
-meshA.fillAll(-1)
-meshB.fillAll(-1)
-
-r = 0.5
-
-r *= np.pi
-
-off = .8
-
-R = [[-1.,-1.],
-     [-1.,1.],
-     [1.,-1.],
-     [1.,-1.]]
-
-R[2][1] += off
-
-for xA,yA,gA,mA in zip(groupA.xc,groupA.yc,groupA.grains,groupA.mats):
-    grain1 = pss.Grain(eqr=gA.radius,shape='polygon',poly_params=R,rot=r)
-    grain1.place(xA,yA,mA,meshA)
-for xB,yB,gB,mB in zip(groupB.xc,groupB.yc,groupB.grains,groupB.mats):
-    grain2 = pss.Grain(eqr=gB.radius,shape='polygon',poly_params=R,rot=r*3)
-    grain2.place(xB,yB,mB,meshB)
-
-meshA.fillAll(9)
-meshB.fillAll(9)
-meshA.blanketVel(-1500.,axis=1)
-meshB.blanketVel(+1500.,axis=1)
-meshD = pss.combine_meshes(meshA,meshB,axis=1)
-meshD.top_and_tail()
-meshD.viewMats()
-meshD.save(fname='regolith_tetrahedracritangle_v3000.iSALE',compress=True)
-meshD.multiplyVels()
-meshD.save(fname='regolith_tetrahedracritangle_v1500.iSALE',compress=True)
-meshD.multiplyVels()
-meshD.save(fname='regolith_tetrahedracritangle_v750.iSALE',compress=True)
-meshA.fillAll(-1)
-meshB.fillAll(-1)
-
-r = 0.5
-
-r *= np.pi
-
-off = 2.
-
-R = [[-1.,-1.],
-     [-1.,1.],
-     [1.,-1.],
-     [1.,-1.]]
-
-R[2][1] += off
-
-for xA,yA,gA,mA in zip(groupA.xc,groupA.yc,groupA.grains,groupA.mats):
-    grain1 = pss.Grain(eqr=gA.radius,shape='polygon',poly_params=R,rot=r*3)
-    grain1.place(xA,yA,mA,meshA)
-for xB,yB,gB,mB in zip(groupB.xc,groupB.yc,groupB.grains,groupB.mats):
-    grain2 = pss.Grain(eqr=gB.radius,shape='polygon',poly_params=R,rot=r)
-    grain2.place(xB,yB,mB,meshB)
-
-meshA.fillAll(9)
-meshB.fillAll(9)
-meshA.blanketVel(-1500.,axis=1)
-meshB.blanketVel(+1500.,axis=1)
-meshE = pss.combine_meshes(meshA,meshB,axis=1)
-meshE.top_and_tail()
-meshE.viewMats()
-meshE.save(fname='regolith_square_v3000.iSALE',compress=True)
-meshE.multiplyVels()
-meshE.save(fname='regolith_square_v1500.iSALE',compress=True)
-meshE.multiplyVels()
-meshE.save(fname='regolith_square_v750.iSALE',compress=True)
-
-meshA.fillAll(-1)
-meshB.fillAll(-1)
-
-r = 0.5
-
-r *= np.pi
-
-
-R = [[-1.,-1.],
-     [-1.,1.],
-     [1.,-1.],
-     [1.,-1.]]
-
-
-for xA,yA,gA,mA in zip(groupA.xc,groupA.yc,groupA.grains,groupA.mats):
-    grain1 = pss.Grain(eqr=gA.radius,shape='polygon',poly_params=R,rot=r)
-    grain1.place(xA,yA,mA,meshA)
-for xB,yB,gB,mB in zip(groupB.xc,groupB.yc,groupB.grains,groupB.mats):
-    grain2 = pss.Grain(eqr=gB.radius,shape='polygon',poly_params=R,rot=r*3)
-    grain2.place(xB,yB,mB,meshB)
-
-meshA.fillAll(9)
-meshB.fillAll(9)
-meshA.blanketVel(-1500.,axis=1)
-meshB.blanketVel(+1500.,axis=1)
-meshF = pss.combine_meshes(meshA,meshB,axis=1)
-meshF.top_and_tail()
-meshF.viewMats()
-meshF.save(fname='regolith_triangle_v3000.iSALE',compress=True)
-meshF.multiplyVels()
-meshF.save(fname='regolith_triangle_v1500.iSALE',compress=True)
-meshF.multiplyVels()
-meshF.save(fname='regolith_triangle_v750.iSALE',compress=True)
-
-meshA.fillAll(-1)
-meshB.fillAll(-1)
-
-import glob
-import random
-
-allfiles = glob.glob('./grain_library/grain_area*.txt')
-
-for xA,yA,gA,mA in zip(groupA.xc,groupA.yc,groupA.grains,groupA.mats):
-    fff = random.choice(allfiles)
-    grain1 = pss.Grain(eqr=gA.radius,shape='file',File=fff)
-    grain1.place(xA,yA,mA,meshA)
-
-for xB,yB,gB,mB in zip(groupB.xc,groupB.yc,groupB.grains,groupB.mats):
-    fff = random.choice(allfiles)
-    grain2 = pss.Grain(eqr=gB.radius,shape='file',File=fff)
-    grain2.place(xB,yB,mB,meshB)
-
-meshA.fillAll(9)
-meshB.fillAll(9)
-meshA.blanketVel(-1500.,axis=1)
-meshB.blanketVel(+1500.,axis=1)
-meshG = pss.combine_meshes(meshA,meshB,axis=1)
-meshG.top_and_tail()
-meshG.viewMats()
-meshG.save(fname='regolith_realistic_v3000.iSALE',compress=True)
-meshG.multiplyVels()
-meshG.save(fname='regolith_realistic_v1500.iSALE',compress=True)
-meshG.multiplyVels()
-meshG.save(fname='regolith_realistic_v750.iSALE',compress=True)
